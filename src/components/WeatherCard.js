0db59ffbf82a992524bfdfa96194fe80d8b346ea
Rasha Moumneh <rasha@fastmail.com>
2019-10-26 19:27:29 -0400

    Return empty card if no city is specified

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -20,15 +20,18 @@ const WeatherCard = (props) => {
   console.log(formatDate(new Date()));
   const date = Date.now
   return (
-    <div className='weather-card box'>
-      {props.data.icon ? <img className='icon' src={`http://openweathermap.org/img/wn/${props.data.icon}@2x.png`} alt='Choose a city!'/> : null}  
+    <div className='box-container'>
+      {props.data.icon ?  
+        <div className='weather-card box'>
+      <img className='icon' src={`http://openweathermap.org/img/wn/${props.data.icon}@2x.png`} alt={props.data.description}/>
       <div className='weather-info'>
         <span className='temp title'>{Math.round(props.data.temp)}&deg;</span>
         <span className='weather-descripton'>{props.data.description}</span>
         <span className='place'>{props.data.place}</span>
         <span className='date'>{formatDate(new Date())}</span>
       </div>
-    </div>
+      </div> : <div className='empty'></div>} 
+      </div>  
   )
 }
 

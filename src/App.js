61848006632f71e3f604531a266a5bdbceff953b
Rasha Moumneh <rasha@fastmail.com>
2019-11-14 17:54:58 -0500

    Centered app on page

--- src/App.js
+++ src/App.js
@@ -1,69 +1,75 @@
-import React, { useState } from 'react';
-import Input from './components/Input';
-import WeatherCard from './components/WeatherCard';
-import axios from 'axios';
-import { reset } from 'ansi-colors';
+import React, { useState } from "react";
+import Input from "./components/Input";
+import WeatherCard from "./components/WeatherCard";
+import axios from "axios";
 
 function App() {
+  const [city, setCity] = useState("");
 
-  const[city, setCity] = useState('') 
-  
-  //weather state from API 
-  const[data, setData] = useState({
-    description: '',
-    temp: '',
-    icon: '',
-    place: '',
-    isFarenheit: false,
-  })
-  const apiKey = 'acb48d94d25a3f640a71be445fa0b204'
-  
-  const handleChange = (event) => {
-    setCity(event.target.value)
-  }
+  //weather state from API
+  const [data, setData] = useState({
+    description: "",
+    temp: "",
+    icon: "",
+    place: "",
+    isFarenheit: false
+  });
+  const apiKey = "acb48d94d25a3f640a71be445fa0b204";
 
-  const toCelcius = (f) => {  
-    if(data.isFarenheit === true) {
-      setData({...data, temp: (f - 32) * 5/9, isFarenheit: false})
-    } 
-  }
+  const handleChange = event => {
+    setCity(event.target.value);
+  };
 
-  const toFarenheit = (c) => {
-    if(data.isFarenheit === false)
-    setData({...data, temp: (c * 9/5) + 32, isFarenheit: true})
-  }
+  const toCelcius = f => {
+    if (data.isFarenheit === true) {
+      setData({ ...data, temp: ((f - 32) * 5) / 9, isFarenheit: false });
+    }
+  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  const toFarenheit = c => {
+    if (data.isFarenheit === false)
+      setData({ ...data, temp: (c * 9) / 5 + 32, isFarenheit: true });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
     axios
-      .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`)
-      .then (res => {
-        const farenheit = (res.data.main.temp - 273.15) * 9/5 + 32
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
+      )
+      .then(res => {
+        const farenheit = ((res.data.main.temp - 273.15) * 9) / 5 + 32;
         setData({
           description: res.data.weather[0].description,
           temp: farenheit,
           icon: res.data.weather[0].icon,
           place: `${res.data.name}, ${res.data.sys.country}`,
           isFarenheit: true
-        }) 
+        });
       })
-      .catch (err => {
-        console.log(err)
-      }, [])
-  }
+      .catch(err => {
+        console.log(err);
+      }, []);
+  };
 
   return (
-    <div className="app-container">
-      <header className='header'>
-        <h1 className='title'>Hey I made a weather app!</h1>
-      </header>
-      <div className='inner-flex-container'>
-        <Input onSubmit={handleSubmit} onChange={handleChange} toCelcius={() => toCelcius(data.temp)} toFarenheit={() => toFarenheit(data.temp)}/>
-        <WeatherCard data={data} />
+    <div className="container">
+      <div className="app-container">
+        <header className="header">
+          <h1 className="title">Hey I made a weather app!</h1>
+        </header>
+        <div className="inner-flex-container">
+          <Input
+            onSubmit={handleSubmit}
+            onChange={handleChange}
+            toCelcius={() => toCelcius(data.temp)}
+            toFarenheit={() => toFarenheit(data.temp)}
+          />
+          <WeatherCard data={data} />
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
-
